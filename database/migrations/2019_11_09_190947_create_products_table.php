51621f42f242b4284361ec28b9ffd6d399370e6c
JuyelRana <mjuyelrana@gmail.com>
2019-11-10 01:42:12 +0600

    Migration created for product and review

--- database/migrations/2019_11_09_190947_create_products_table.php
+++ database/migrations/2019_11_09_190947_create_products_table.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateProductsTable extends Migration
 {
-    /**
+    /**   
      * Run the migrations.
      *
      * @return void
@@ -15,6 +15,12 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('image')->nullable();
+            $table->text('description');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->integer('discount');
             $table->timestamps();
         });
     }
